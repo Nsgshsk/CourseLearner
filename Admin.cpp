41d1619a5c47d8190936008085f58189c999421f
Lubomir Topalski <lubomirtopalski@gmail.com>
2025-05-28 17:39:43 +0300

    Created more Users

---
+++ Admin.cpp
@@ -0,0 +1,13 @@
+﻿#include "Admin.h"
+
+void Admin::generate_id()
+{
+    setId(count);
+    count++;
+}
+
+Admin::Admin(const String& first_name, const String& last_name, const String& password) :
+User(first_name, last_name, password)
+{
+    generate_id();
+}
